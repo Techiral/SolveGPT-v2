d897c00699d478df2718ba50f5d73cd6c54d19f7
Techiral <115097129+Techiral@users.noreply.github.com>
2023-06-22 10:47:09 +0530

    Update __init__.py

--- apps/__init__.py
+++ apps/__init__.py
@@ -31,6 +31,7 @@ def create_app():
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(views)
 
     from .models import User, db
 
