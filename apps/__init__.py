271ecd0c8d1e29704c03c42dc8734757389ad4f7
Techiral <115097129+Techiral@users.noreply.github.com>
2023-06-22 10:40:16 +0530

    Update __init__.py

--- apps/__init__.py
+++ apps/__init__.py
@@ -32,7 +32,7 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User
+    from .models import User, db
 
     create_database()
 
@@ -49,6 +49,5 @@ def create_app():
 
 def create_database():
     with current_app.app_context():
-        if not path.exists('website/' + DB_NAME):
-            db.create_all()
-            print('Created Database!')
+        db.create_all()
+        print('Created Database!')
